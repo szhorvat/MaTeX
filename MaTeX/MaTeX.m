0d8548dd4c37aa14cd65dac0b73924edcd04163c
Szabolcs Horvát <szhorvat@gmail.com>
2016-08-18 16:27:24 +0200

    version 1.6.0pre

--- MaTeX/MaTeX.m
+++ MaTeX/MaTeX.m
@@ -4,7 +4,7 @@
 (* :Title: MaTeX *)
 (* :Author: Szabolcs Horvát <szhorvat@gmail.com> *)
 (* :Context: MaTeX` *)
-(* :Version: 1.5.0 *)
+(* :Version: 1.6.0pre *)
 (* :Date: 2015-03-04 *)
 
 (* :Mathematica Version: 10.0 *)
@@ -40,7 +40,7 @@ ConfigureMaTeX[] returns the current configuration.";
 
 ClearMaTeXCache::usage = "ClearMaTeXCache[] clears MaTeX's cache.";
 
-`Developer`$Version = "1.5.0 (June 19, 2016)";
+`Developer`$Version = "1.6.0pre";
 
 Begin["`Private`"] (* Begin Private Context *)
 
