8d302409f13854fee88e75104b5f85f3c8bdc36c
Szabolcs Horvát <szhorvat@gmail.com>
2016-11-15 11:53:19 +0100

    Add PacletInfo.m

---
+++ MaTeX/PacletInfo.m
@@ -0,0 +1,20 @@
+(* ::Package:: *)
+
+(* Paclet Info File *)
+
+(* created 2016/11/13*)
+
+Paclet[
+    Name -> "MaTeX",
+    Version -> "1.6.2",
+    MathematicaVersion -> "10.0+",
+    Description -> "Create LaTeX-typeset labels within Mathematica.",
+    Creator -> "Szabolcs Horv\[AAcute]t",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English", MainPage -> "Guides/MaTeX"}, 
+            {"Kernel", Root -> ".", Context -> "MaTeX`"}
+        }
+]
+
+
