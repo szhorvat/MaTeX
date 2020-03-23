3109240f0154dbf9504774c4298e7bed784b18c8
Szabolcs Horvát <szhorvat@gmail.com>
2020-03-23 21:07:09 +0100

    Update PacletInfo

--- MaTeX/PacletInfo.m
+++ MaTeX/PacletInfo.m
@@ -2,12 +2,14 @@
 
 Paclet[
   Name -> "MaTeX",
-  Version -> "1.7.5",
+  Version -> "1.7.6",
   MathematicaVersion -> "10.0+",
   Description -> "Create LaTeX-typeset labels within Mathematica.",
+  "Keywords" -> {"LaTeX", "Typesetting", "Graphics"},
   Creator -> "Szabolcs Horv\[AAcute]t <szhorvat@gmail.com>",
   URL -> "http://szhorvat.net/mathematica/MaTeX",
   Thumbnail -> "Logo.png",
+  "Icon" -> "Logo.png", (* M12.1+ uses Icon instead of Thumbnail. *)
   Extensions ->
       {
         {"Documentation", Language -> All, MainPage -> "Guides/MaTeX"},
